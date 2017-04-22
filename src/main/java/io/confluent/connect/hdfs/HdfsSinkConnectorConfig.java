e06f68a5d8deb5ce9595ddc90e7b15195041b5bf
Konstantine Karantasis <konstantine@confluent.io>
2017-04-22 15:30:25 -0700

    Add individual configs. Need to overwrite storage.class default.

--- src/main/java/io/confluent/connect/hdfs/HdfsSinkConnectorConfig.java
+++ src/main/java/io/confluent/connect/hdfs/HdfsSinkConnectorConfig.java
@@ -14,17 +14,25 @@
 
 package io.confluent.connect.hdfs;
 
+import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.config.ConfigDef.Width;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import io.confluent.connect.storage.StorageSinkConnectorConfig;
+import io.confluent.connect.storage.common.ComposableConfig;
+import io.confluent.connect.storage.common.StorageCommonConfig;
+import io.confluent.connect.storage.hive.HiveConfig;
+import io.confluent.connect.storage.partitioner.PartitionerConfig;
 
 public class HdfsSinkConnectorConfig extends StorageSinkConnectorConfig {
 
@@ -89,51 +97,196 @@ public class HdfsSinkConnectorConfig extends StorageSinkConnectorConfig {
   private static final String KERBEROS_TICKET_RENEW_PERIOD_MS_DISPLAY = "Kerberos Ticket Renew Period (ms)";
 
   // Internal group
-  public static final String STORAGE_CLASS_CONFIG = "storage.class";
-  public static final String STORAGE_CLASS_DOC =
-      "The underlying storage layer. The default is HDFS.";
+  // Need to just set the default
   public static final String STORAGE_CLASS_DEFAULT = "io.confluent.connect.hdfs.storage.HdfsStorage";
-  public static final String STORAGE_CLASS_DISPLAY = "Storage Class";
-
-  public static final String HDFS_GROUP = "HDFS";
-  public static final String SECURITY_GROUP = "Security";
-  public static final String INTERNAL_GROUP = "Internal";
 
   private static final ConfigDef.Recommender hdfsAuthenticationKerberosDependentsRecommender = new BooleanParentRecommender(HDFS_AUTHENTICATION_KERBEROS_CONFIG);
 
+  private final String name;
+
+  private final StorageCommonConfig commonConfig;
+  private final HiveConfig hiveConfig;
+  private final PartitionerConfig partitionerConfig;
+
+  private final Map<String, ComposableConfig> propertyToConfig = new HashMap<>();
+  private final Set<AbstractConfig> allConfigs = new HashSet<>();
+
   static {
     // Define HDFS configuration group
+    {
+      final String group = "HDFS";
+      int orderInGroup = 0;
 
-    // HDFS_URL_CONFIG property is retained for backwards compatibility with HDFS connector and will be removed in future versions.
-    CONFIG_DEF.define(HDFS_URL_CONFIG,
-        Type.STRING,
-        HDFS_URL_DEFAULT,
-        Importance.HIGH,
-        HDFS_URL_DOC,
-        group,
-        ++orderInGroup,
-        Width.MEDIUM,
-        HDFS_URL_DISPLAY);
+      // HDFS_URL_CONFIG property is retained for backwards compatibility with HDFS connector and
+      // will be removed in future versions.
+      CONFIG_DEF.define(
+          HDFS_URL_CONFIG,
+          Type.STRING,
+          HDFS_URL_DEFAULT,
+          Importance.HIGH,
+          HDFS_URL_DOC,
+          group,
+          ++orderInGroup,
+          Width.MEDIUM,
+          HDFS_URL_DISPLAY
+      );
 
-    CONFIG_DEF.define(HADOOP_CONF_DIR_CONFIG, Type.STRING, HADOOP_CONF_DIR_DEFAULT, Importance.HIGH, HADOOP_CONF_DIR_DOC, HDFS_GROUP, 2, Width.MEDIUM, HADOOP_CONF_DIR_DISPLAY)
-        .define(HADOOP_HOME_CONFIG, Type.STRING, HADOOP_HOME_DEFAULT, Importance.HIGH, HADOOP_HOME_DOC, HDFS_GROUP, 3, Width.SHORT, HADOOP_HOME_DISPLAY)
-        .define(LOGS_DIR_CONFIG, Type.STRING, LOGS_DIR_DEFAULT, Importance.HIGH, LOGS_DIR_DOC, HDFS_GROUP, 5, Width.SHORT, LOGS_DIR_DISPLAY)
+      CONFIG_DEF.define(
+          HADOOP_CONF_DIR_CONFIG,
+          Type.STRING,
+          HADOOP_CONF_DIR_DEFAULT,
+          Importance.HIGH,
+          HADOOP_CONF_DIR_DOC,
+          group,
+          ++orderInGroup,
+          Width.MEDIUM,
+          HADOOP_CONF_DIR_DISPLAY
+      );
 
-    // Define Security configuration group
-    CONFIG_DEF.define(HDFS_AUTHENTICATION_KERBEROS_CONFIG, Type.BOOLEAN, HDFS_AUTHENTICATION_KERBEROS_DEFAULT, Importance.HIGH, HDFS_AUTHENTICATION_KERBEROS_DOC,
-        SECURITY_GROUP, 1, Width.SHORT, HDFS_AUTHENTICATION_KERBEROS_DISPLAY,
-        Arrays.asList(CONNECT_HDFS_PRINCIPAL_CONFIG, CONNECT_HDFS_KEYTAB_CONFIG, HDFS_NAMENODE_PRINCIPAL_CONFIG, KERBEROS_TICKET_RENEW_PERIOD_MS_CONFIG))
-        .define(CONNECT_HDFS_PRINCIPAL_CONFIG, Type.STRING, CONNECT_HDFS_PRINCIPAL_DEFAULT, Importance.HIGH, CONNECT_HDFS_PRINCIPAL_DOC,
-            SECURITY_GROUP, 2, Width.MEDIUM, CONNECT_HDFS_PRINCIPAL_DISPLAY, hdfsAuthenticationKerberosDependentsRecommender)
-        .define(CONNECT_HDFS_KEYTAB_CONFIG, Type.STRING, CONNECT_HDFS_KEYTAB_DEFAULT, Importance.HIGH, CONNECT_HDFS_KEYTAB_DOC,
-            SECURITY_GROUP, 3, Width.MEDIUM, CONNECT_HDFS_KEYTAB_DISPLAY, hdfsAuthenticationKerberosDependentsRecommender)
-        .define(HDFS_NAMENODE_PRINCIPAL_CONFIG, Type.STRING, HDFS_NAMENODE_PRINCIPAL_DEFAULT, Importance.HIGH, HDFS_NAMENODE_PRINCIPAL_DOC,
-            SECURITY_GROUP, 4, Width.MEDIUM, HDFS_NAMENODE_PRINCIPAL_DISPLAY, hdfsAuthenticationKerberosDependentsRecommender)
-        .define(KERBEROS_TICKET_RENEW_PERIOD_MS_CONFIG, Type.LONG, KERBEROS_TICKET_RENEW_PERIOD_MS_DEFAULT, Importance.LOW, KERBEROS_TICKET_RENEW_PERIOD_MS_DOC,
-            SECURITY_GROUP, 5, Width.SHORT, KERBEROS_TICKET_RENEW_PERIOD_MS_DISPLAY, hdfsAuthenticationKerberosDependentsRecommender);
+      CONFIG_DEF.define(
+          HADOOP_HOME_CONFIG,
+          Type.STRING,
+          HADOOP_HOME_DEFAULT,
+          Importance.HIGH,
+          HADOOP_HOME_DOC,
+          group,
+          ++orderInGroup,
+          Width.SHORT,
+          HADOOP_HOME_DISPLAY
+      );
 
-    // Define Internal configuration group
-    CONFIG_DEF.define(STORAGE_CLASS_CONFIG, Type.STRING, STORAGE_CLASS_DEFAULT, Importance.LOW, STORAGE_CLASS_DOC, INTERNAL_GROUP, 1, Width.MEDIUM, STORAGE_CLASS_DISPLAY);
+      CONFIG_DEF.define(
+          LOGS_DIR_CONFIG,
+          Type.STRING,
+          LOGS_DIR_DEFAULT,
+          Importance.HIGH,
+          LOGS_DIR_DOC,
+          group,
+          ++orderInGroup,
+          Width.SHORT,
+          LOGS_DIR_DISPLAY
+      );
+    }
+
+    {
+      final String group = "Security";
+      int orderInGroup = 0;
+      // Define Security configuration group
+      CONFIG_DEF.define(
+          HDFS_AUTHENTICATION_KERBEROS_CONFIG,
+          Type.BOOLEAN,
+          HDFS_AUTHENTICATION_KERBEROS_DEFAULT,
+          Importance.HIGH,
+          HDFS_AUTHENTICATION_KERBEROS_DOC,
+          group,
+          ++orderInGroup,
+          Width.SHORT,
+          HDFS_AUTHENTICATION_KERBEROS_DISPLAY,
+          Arrays.asList(
+              CONNECT_HDFS_PRINCIPAL_CONFIG,
+              CONNECT_HDFS_KEYTAB_CONFIG,
+              HDFS_NAMENODE_PRINCIPAL_CONFIG,
+              KERBEROS_TICKET_RENEW_PERIOD_MS_CONFIG
+          )
+      );
+
+      CONFIG_DEF.define(
+          CONNECT_HDFS_PRINCIPAL_CONFIG,
+          Type.STRING,
+          CONNECT_HDFS_PRINCIPAL_DEFAULT,
+          Importance.HIGH,
+          CONNECT_HDFS_PRINCIPAL_DOC,
+          group,
+          ++orderInGroup,
+          Width.MEDIUM,
+          CONNECT_HDFS_PRINCIPAL_DISPLAY,
+          hdfsAuthenticationKerberosDependentsRecommender
+      );
+
+      CONFIG_DEF.define(
+          CONNECT_HDFS_KEYTAB_CONFIG,
+          Type.STRING,
+          CONNECT_HDFS_KEYTAB_DEFAULT,
+          Importance.HIGH,
+          CONNECT_HDFS_KEYTAB_DOC,
+          group,
+          ++orderInGroup,
+          Width.MEDIUM,
+          CONNECT_HDFS_KEYTAB_DISPLAY,
+          hdfsAuthenticationKerberosDependentsRecommender
+      );
+
+      CONFIG_DEF.define(
+          HDFS_NAMENODE_PRINCIPAL_CONFIG,
+          Type.STRING,
+          HDFS_NAMENODE_PRINCIPAL_DEFAULT,
+          Importance.HIGH,
+          HDFS_NAMENODE_PRINCIPAL_DOC,
+          group,
+          ++orderInGroup,
+          Width.MEDIUM,
+          HDFS_NAMENODE_PRINCIPAL_DISPLAY,
+          hdfsAuthenticationKerberosDependentsRecommender
+      );
+
+      CONFIG_DEF.define(
+          KERBEROS_TICKET_RENEW_PERIOD_MS_CONFIG,
+          Type.LONG,
+          KERBEROS_TICKET_RENEW_PERIOD_MS_DEFAULT,
+          Importance.LOW,
+          KERBEROS_TICKET_RENEW_PERIOD_MS_DOC,
+          group,
+          ++orderInGroup,
+          Width.SHORT,
+          KERBEROS_TICKET_RENEW_PERIOD_MS_DISPLAY,
+          hdfsAuthenticationKerberosDependentsRecommender
+      );
+    }
+
+  }
+
+  public HdfsSinkConnectorConfig(Map<String, String> props) {
+    this(CONFIG_DEF, props);
+  }
+
+  protected HdfsSinkConnectorConfig(ConfigDef configDef, Map<String, String> props) {
+    super(configDef, props);
+    commonConfig = new StorageCommonConfig(originalsStrings());
+    hiveConfig = new HiveConfig(originalsStrings());
+    partitionerConfig = new PartitionerConfig(originalsStrings());
+    this.name = parseName(originalsStrings());
+    addToGlobal(hiveConfig);
+    addToGlobal(partitionerConfig);
+    addToGlobal(commonConfig);
+    addToGlobal(this);
+  }
+
+  private void addToGlobal(AbstractConfig config) {
+    allConfigs.add(config);
+    addConfig(config.values(), (ComposableConfig) config);
+  }
+
+  private void addConfig(Map<String, ?> parsedProps, ComposableConfig config) {
+    for (String key : parsedProps.keySet()) {
+      propertyToConfig.put(key, config);
+    }
+  }
+
+  protected static String parseName(Map<String, String> props) {
+    String nameProp = props.get("name");
+    return nameProp != null ? nameProp : "S3-sink";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Map<String, ?> plainValues() {
+    Map<String, Object> map = new HashMap<>();
+    for (AbstractConfig config : allConfigs) {
+      map.putAll(config.values());
+    }
+    return map;
   }
 
   private static class BooleanParentRecommender implements ConfigDef.Recommender {
@@ -156,10 +309,21 @@ public class HdfsSinkConnectorConfig extends StorageSinkConnectorConfig {
   }
 
   public static ConfigDef getConfig() {
-    return CONFIG_DEF;
-  }
+    Map<String, ConfigDef.ConfigKey> everything = new HashMap<>(CONFIG_DEF.configKeys());
+    everything.putAll(StorageCommonConfig.getConfig().configKeys());
+    everything.putAll(PartitionerConfig.getConfig().configKeys());
 
-  public HdfsSinkConnectorConfig(Map<String, String> props) {
-    super(props);
+    Set<String> blacklist = new HashSet<>();
+    blacklist.add(StorageSinkConnectorConfig.ROTATE_INTERVAL_MS_CONFIG);
+    blacklist.add(StorageSinkConnectorConfig.ROTATE_SCHEDULE_INTERVAL_MS_CONFIG);
+    blacklist.add(StorageSinkConnectorConfig.SHUTDOWN_TIMEOUT_CONFIG);
+
+    ConfigDef visible = new ConfigDef();
+    for (ConfigDef.ConfigKey key : everything.values()) {
+      if(!blacklist.contains(key.name)) {
+        visible.define(key);
+      }
+    }
+    return visible;
   }
 }
